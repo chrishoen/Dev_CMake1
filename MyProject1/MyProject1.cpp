462f40a760c6259bd482fa534c16f5444c9edb2c
chrishoen <chris123hoen@gmail.com>
2019-01-01 09:38:29 -0500

    Add project files.

---
+++ MyProject1/MyProject1.cpp
@@ -0,0 +1,12 @@
+﻿// MyProject1.cpp : Defines the entry point for the application.
+//
+
+#include "MyProject1.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
