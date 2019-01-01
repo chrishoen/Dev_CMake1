c800fda1b6ab9c3b13223644f61af22ffdbd39d7
chrishoen <chris123hoen@gmail.com>
2019-01-01 09:58:34 -0500

    first commit

--- MyProject1/MyProject1.h
+++
@@ -1,8 +0,0 @@
-﻿// MyProject1.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
---
+++ MyProject1/main.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+#include "MyClass1.h"
+#include "MyFunction1.h"
+
+int main(int argc, char** argv)
+{
+   printf("main\n");
+   MyClass1 tMyClass1;
+   tMyClass1.doSomething();
+   doMyFunction1();
+   return 0;
+}
