c3b6b8c13da7e61868c25404f9634c8552c80ae5
chrishoen <chris123hoen@gmail.com>
2021-12-31 14:03:27 -0500

    more

--- MySpecial.cmake
+++ MySpecial.cmake
@@ -4,16 +4,23 @@
 
 function(my_special1)
 
-   message(STATUS "my_special***********************************************************BEGIN")
+   message(STATUS "my_special1***********************************************************BEGIN")
    if(MSVC)
       message(STATUS "my_special MSVC")
    elseif(NOT CMAKE_SYSTEM_VERSION EQUAL 101)
       message(STATUS "my_special LINUX")
-   else()
+   elseif(CMAKE_SYSTEM_VERSION EQUAL 101)
       message(STATUS "my_special LINUX BEAGLE")
+   elseif(CMAKE_SYSTEM_VERSION EQUAL 201)
+      message(STATUS "my_special LINUX DART")
+   else()
+      message(STATUS "my_special LINUX UBUNTU")
    endif()
 
-   message(STATUS "my_special***********************************************************END")
+   message(STATUS "CMAKE_INCLUDE_PATH" ${CMAKE_INCLUDE_PATH})
+   message(STATUS "INCLUDE_DIRECTORIES" ${INCLUDE_DIRECTORIES})
+
+   message(STATUS "my_special1***********************************************************END")
 endfunction()
 
 #*******************************************************************************
