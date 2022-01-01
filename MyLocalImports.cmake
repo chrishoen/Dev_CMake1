11525f4ff0a99eb71e7ebbda1b4b1f3d3e5d1b7e
chrishoen <chris123hoen@gmail.com>
2022-01-01 15:00:30 -0500

    switched to beagle

--- MyLocalImports.cmake
+++ MyLocalImports.cmake
@@ -4,7 +4,7 @@
 
 function(my_lib_import_MyStaticLib _target)
 
-   target_link_libraries(${_target} MyStaticLib)
+#   target_link_libraries(${_target} MyStaticLib)
 
 endfunction()
 
@@ -13,4 +13,7 @@ function(my_inc_import_MyStaticLib _target)
    target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:MyStaticLib,INTERFACE_INCLUDE_DIRECTORIES>)
 
 endfunction()
+
+
+
 
