cd7ef5957bf888db61251bac9ee52ab39efd1044
chrishoen <chris123hoen@gmail.com>
2022-01-01 15:14:47 -0500

    added MySharedLib

--- MyLocalImports.cmake
+++ MyLocalImports.cmake
@@ -4,13 +4,29 @@
 
 function(my_lib_import_MyStaticLib _target)
 
-#   target_link_libraries(${_target} MyStaticLib)
+   target_link_libraries(${_target} MyStaticLib)
 
 endfunction()
 
 function(my_inc_import_MyStaticLib _target)
 
    target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:MyStaticLib,INTERFACE_INCLUDE_DIRECTORIES>)
+
+endfunction()
+
+#*******************************************************************************
+#*******************************************************************************
+#*******************************************************************************
+
+function(my_lib_import_MySharedLib _target)
+
+   target_link_libraries(${_target} MySharedLib)
+
+endfunction()
+
+function(my_inc_import_MySharedLib _target)
+
+   target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:MySharedLib,INTERFACE_INCLUDE_DIRECTORIES>)
 
 endfunction()
 
---
+++ MyProgram3/main.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#include "MyFunction2.h"
+
+int main(int argc, char** argv)
+{
+   printf("Hello World - MyProgram3\n");
+   void doFunction2();
+   return 0;
+}
