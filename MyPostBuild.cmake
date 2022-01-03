e7440e314edd34bcc291f531615976b705c35739
chrishoen <chris123hoen@gmail.com>
2022-01-02 20:18:59 -0500

    more

--- MyPostBuild.cmake
+++ MyPostBuild.cmake
@@ -2,7 +2,7 @@
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_post_build_copy_bin _target)
+function(my_post_build_scp_bin _target)
 
    set(_command scp -p ${_target} ${MyTargetHostName}:${MyCopyBinToDir})
    add_custom_command(
@@ -34,7 +34,7 @@ endfunction()
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_post_build_copy_lib _target)
+function(my_post_build_scp_lib _target)
 
    set(_command scp -p lib${_target}.so ${MyTargetHostName}:${MyCopyLibToDir})
    add_custom_command(
@@ -60,18 +60,69 @@ function(my_post_build_copy_lib _target)
       COMMAND ${_command}
       VERBATIM)
 
-   return()
+endfunction()
 
+#*******************************************************************************
+#*******************************************************************************
+#*******************************************************************************
 
+function(my_post_build_copy_bin _target)
+
+   set(_command cp -p ${_target} ${MyCopyBinToDir})
    add_custom_command(
       TARGET  ${_target}
       POST_BUILD
-      COMMAND scp lib${_target}.so ${MyTargetHostName}:/opt/prime/lib)
-
+      COMMAND echo ${_command}
+      VERBATIM)
    add_custom_command(
       TARGET  ${_target}
       POST_BUILD
-      COMMAND ssh ${MyTargetHostName} "sudo chmod 777 /opt/prime/lib/lib${_target}.so")
+      COMMAND ${_command}
+      VERBATIM)
+
+   set(_command ${MySudo}chmod 777 ${MyCopyBinToDir}/${_target})
+   add_custom_command(
+      TARGET  ${_target}
+      POST_BUILD
+      COMMAND echo ${_command}
+      VERBATIM)
+   add_custom_command(
+      TARGET  ${_target}
+      POST_BUILD
+      COMMAND ${_command}
+      VERBATIM)
+
+endfunction()
+
+#*******************************************************************************
+#*******************************************************************************
+#*******************************************************************************
+
+function(my_post_build_copy_lib _target)
+
+   set(_command cp -p lib${_target}.so ${MyCopyLibToDir})
+   add_custom_command(
+      TARGET  ${_target}
+      POST_BUILD
+      COMMAND echo ${_command}
+      VERBATIM)
+   add_custom_command(
+      TARGET  ${_target}
+      POST_BUILD
+      COMMAND ${_command}
+      VERBATIM)
+
+   set(_command ${MySudo}chmod 777 ${MyCopyLibToDir}/lib${_target}.so)
+   add_custom_command(
+      TARGET  ${_target}
+      POST_BUILD
+      COMMAND echo ${_command}
+      VERBATIM)
+   add_custom_command(
+      TARGET  ${_target}
+      POST_BUILD
+      COMMAND ${_command}
+      VERBATIM)
 
 endfunction()
 
