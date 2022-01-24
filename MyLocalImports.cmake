28c09d709c023e27533a6a61dcce0103af9e491f
chrishoen <chris123hoen@gmail.com>
2022-01-24 10:37:06 -0500

    using new format

--- MyLocalImports.cmake
+++ MyLocalImports.cmake
@@ -2,34 +2,15 @@
 #*******************************************************************************
 #*******************************************************************************
 
-function(my_lib_import_MyStaticLib _target)
+function(my_lib_import_CommonLib _target)
 
-   target_link_libraries(${_target} MyStaticLib)
-
-endfunction()
-
-function(my_inc_import_MyStaticLib _target)
-
-   target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:MyStaticLib,INTERFACE_INCLUDE_DIRECTORIES>)
+   target_link_libraries(${_target} CommonLib)
 
 endfunction()
 
-#*******************************************************************************
-#*******************************************************************************
-#*******************************************************************************
+function(my_inc_import_CommonLib _target)
 
-function(my_lib_import_MySharedLib _target)
-
-   target_link_libraries(${_target} MySharedLib)
+   target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:CommonLib,INTERFACE_INCLUDE_DIRECTORIES>)
 
 endfunction()
-
-function(my_inc_import_MySharedLib _target)
-
-   target_include_directories(${_target} PRIVATE $<TARGET_PROPERTY:MySharedLib,INTERFACE_INCLUDE_DIRECTORIES>)
-
-endfunction()
-
-
-
 
